37d0aa4d432840a16b4231b2ce3a14db1de24526
lokeshwitten <gurorkrupa@gmail.com>
2022-04-28 01:33:10 +0530

    Dummy

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('nodecomplete', 'root', 'Lokesh0401@', {
     dialect: 'mysql',
-    host: 'ec2-13-233-153-138.ap-south-1.compute.amazonaws.com'
+    host: 'localhost'
 });
 
 module.exports = sequelize;
