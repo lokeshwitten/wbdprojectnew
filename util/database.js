8f4728b22939b5544407a3b6015a6f040ab2cc3a
lokeshwitten <gurorkrupa@gmail.com>
2022-04-27 19:30:50 +0530

    Added CI/CD flows

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('nodecomplete', 'root', 'Lokesh0401@', {
     dialect: 'mysql',
-    host: 'localhost'
+    host: 'ec2-13-233-153-138.ap-south-1.compute.amazonaws.com'
 });
 
 module.exports = sequelize;
